8fe82b754f673cdf789669b5cfa71699412f2a90
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-05-16 08:31:17 +0700

    style: fix title

--- resources/views/admin/layout/template.blade.php
+++ resources/views/admin/layout/template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>{{ ucfirst(request()->route()->getName()) }}</title>
+    <title>{{ Str::before(ucfirst(request()->route()->getName()), '.') }}</title>
 
     <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.png') }}">
 
