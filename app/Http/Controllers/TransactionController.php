c09b46216ba49e6d16be7ecdf84b8f54891399e7
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-05-18 13:49:08 +0700

    update: stocks product

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,22 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use App\Models\Product;
 use App\Models\Shipment;
 use App\Models\Transaction;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
     public function update_payment_status(Int $id)
     {
-        $transaction = Transaction::where('id', $id)->first();
-        $transaction->status = "done";
-        $transaction->save();
+        DB::beginTransaction();
 
-        $shipment = Shipment::where('id', $transaction->order->shipment->id)->first();
-        $shipment->status = "preparation for shipping";
-        $shipment->save();
+        try {
+            $transaction = Transaction::where('id', $id)->first();
+            $transaction->status = "done";
+            $transaction->save();
 
-        return redirect()->back()->with("order", "Your payment has been paid successfully!");
+            $shipment = Shipment::where('id', $transaction->order->shipment->id)->first();
+            $shipment->status = "preparation for shipping";
+            $shipment->save();
+
+            throw new \Exception('Intentional error for testing');
+
+            $product = Product::where("id", $transaction->order->product->id)->first();
+            $product->stock -= $transaction->order->amount;
+            $product->save();
+
+            DB::commit();
+            return redirect()->back()->with("order", "Your payment has been paid successfully!");
+        } catch(Exception) {
+            DB::rollback();
+            return redirect()->back()->with('server_error', '500: Server error');
+        }
     }
 }
